41c821f252c487b241637a793e8412e163cab701
padouciel <padouciel@gmail.com>
2015-08-05 17:53:17 +0200

    entrypoint nas

--- firebird/fbclient-run.sh
+++ firebird/fbclient-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name fbclient --rm  --link fbserver:fbserver  --volumes-from fbserver -ti --entrypoint=/bin/bash padouciel/firebird
+docker run --name fbclient --rm  --link fbserver:fbserver  --volumes-from fbserver -ti --entrypoint=/bin/bash -u root:root padouciel/firebird
--- firebird/fbserver-run.sh
+++ firebird/fbserver-run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+docker rm fbserver > /dev/null 2>&1
+
+# le path de montage DOIT correspondre à la variable FB_DB_PATH init dans le Dockerfile...
 docker run --name fbserver -p 3050:3050 -v /var/lib/firebird:/srv/firebird -d padouciel/firebird
