d837599e3aeaefe9ea6cfde8f944d8771bd6edbe
padouciel <padouciel@gmail.com>
2015-08-28 08:36:18 +0200

    New version of nas-server image

--- novaxel_nas-server/entrypoint.sh
+++ novaxel_nas-server/entrypoint.sh
@@ -10,30 +10,55 @@ function cleanup()
 	# On arrive ici après le trap
 	echo "Stopping NAS"
 
-	killall -w novaappserver || "Error ${?}"
-	# no matter...
-	killall Xvfb
+	killall -w novaappserver || echo "Error ${?}"
 
-	echo "Exited ${?}"
+	# no matter to wait for the process end...
+	killall Xvfb || echo "Error : ?{1}"
+
+	killall -w fbguard || "Error ${?}"
+
+	echo "Exited"
 }
 
 # Pour reprendre la main si on stop le container...
 trap "cleanup" HUP INT QUIT KILL TERM
 
-# TODO : tester présente serveur FB...
 
 # On vérifie si des bases domain/event sont présentes
-if [[ -z "${FBSERVER_ENV_FB_DB_PATH}" ]]
+if [[ -z "${NAS_DB_PATH_DOMAIN}" ]]
 then
-	echo "La variable d'environnement FBSERVER_ENV_FB_DB_PATH doit-être initialisée pour que le NAS sache où chercher ses bases..."
+	echo "The environnement variable NAS_DB_PATH_DOMAIN must be set (in Dockerfile)"
 	exit 1
+else
+	 [[ ! -f "${NAS_DB_PATH_DOMAIN}/domain.fdb" ]] && CREATE_DB_DOM=0
+	 [[ ! -f "${NAS_DB_PATH_DOMAIN}/event.fdb" ]] && CREATE_DB_EVENT=0
 fi
 
-# TODO Création des bases domain/event si non présentes
+echo "Starting Firebird"
+runuser firebird -s /bin/sh -c "/usr/sbin/fbguard -pidfile /var/run/firebird/firebird.pid -daemon -forever"
+
+if [[ -n "${CREATE_DB_DOM}" ]]
+then
+	mkdir -p  "${NAS_DB_PATH_DOMAIN}" && chown -R firebird:firebird "${NAS_DB_PATH_DOMAIN}"
+	echo "CREATE DATABASE '${NAS_DB_PATH_DOMAIN}/domain.fdb' PAGE_SIZE 16384 DEFAULT CHARACTER SET UTF8;"|isql-fb -user sysdba -password masterkey
+	isql-fb -user sysdba -password masterkey -b -i /opt/novaxel/sql/domain.sql "'${NAS_DB_PATH_DOMAIN}/domain.fdb"
+fi
+
+if [[ -n "${CREATE_DB_EVENT}" ]]
+then
+	mkdir -p  "${NAS_DB_PATH_DOMAIN}"  && chown -R firebird:firebird "${NAS_DB_PATH_DOMAIN}"
+	echo "CREATE DATABASE '${NAS_DB_PATH_DOMAIN}/event.fdb' PAGE_SIZE 16384 DEFAULT CHARACTER SET UTF8;"|isql-fb -user sysdba -password masterkey
+	isql-fb -user sysdba -password masterkey -b -i /opt/novaxel/sql/event.sql "'${NAS_DB_PATH_DOMAIN}/event.fdb"
+fi
+
+# update NAS conf with the right env variable...
+sed -i -e 's|\(DOMAIN_DATABASE_URL=\)\(.*\)|\1localhost:'"${NAS_DB_PATH_DOMAIN}"'/domain.fdb|' /opt/novaxel/novaappserver.conf
+sed -i -e 's|\(EVENT_DATABASE_URL=\)\(.*\)|\1localhost:'"${NAS_DB_PATH_DOMAIN}"'/event.fdb|' /opt/novaxel/novaappserver.conf
+
 
 # Lancement du serveur NAS en arrière plan
 echo "Starting NAS"
-/opt/novaxel/novaappserver/novaappserver.sh
+/opt/novaxel/novaappserver/novaappserver.sh -c /opt/novaxel/novaappserver.conf
 
 # Récupération du PID 
 NAS_PID=$(pgrep novaappserver)
