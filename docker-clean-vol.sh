31402870d115b8acf48d4c92746f4b8f232963f3
padouciel <padouciel@gmail.com>
2015-09-20 18:10:11 +0200

    Volume bug

--- docker-clean-vol.sh
+++ docker-clean-vol.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes "$1"
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes $1
